ab270f4423e323b7f7f0e705255b706e40ca4ca8
Suncica Paroski <suncica.paroski@gmail.com>
2026-02-16 14:47:58 +0100

    Initial Commit

---
+++ MozzartApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MozzartApp
+//
+//  Created by suncica on 16. 2. 2026..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MozzartApp/MozzartAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MozzartAppApp.swift
+//  MozzartApp
+//
+//  Created by suncica on 16. 2. 2026..
+//
+
+import SwiftUI
+
+@main
+struct MozzartAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
